35097ae5c5d8c8c346f85d9076f911dc0daa0ca1
ViskCY <109294364+ViskCY@users.noreply.github.com>
2024-08-01 01:47:26 +0300

    cv locale check

--- app/Http/Controllers/CVController.php
+++ app/Http/Controllers/CVController.php
@@ -8,7 +8,14 @@ class CVController extends Controller
 {
     public function index()
     {
-        $markdownContent = file_get_contents(public_path('storage/cv.md'));
+        $locale = App::getLocale();
+        $filePath = public_path("storage/{$locale}/cv.md");
+        
+        if (!file_exists($filePath)) {
+            abort(404, "CV file not found for locale: {$locale}");
+        }
+        
+        $markdownContent = file_get_contents(public_path("storage/{$locale}/cv.md"));
         $converter = new MarkdownConverter($markdownContent);
         $markdown = $converter->toHtml();
 
