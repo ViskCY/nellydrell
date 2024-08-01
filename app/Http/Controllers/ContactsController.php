2a50b1d334d43b4cfb63509f53ced68638b4bf82
ViskCY <109294364+ViskCY@users.noreply.github.com>
2024-08-01 22:27:16 +0300

    contacts controller locale

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\App;
 use App\Http\Controllers\MarkdownConverter;
 
 class ContactsController extends Controller
 {
     public function index()
     {
-        $markdownContent = file_get_contents(public_path('storage/contacts.md'));
+        $locale = App::getLocale();
+        $filePath = public_path("storage/{$locale}/contacts.md");
+        
+        if (!file_exists($filePath)) {
+            abort(404, "Contacts file not found for locale: {$locale}");
+        }
+        
+        $markdownContent = file_get_contents(public_path("storage/{$locale}/contacts.md"));
         $converter = new MarkdownConverter($markdownContent);
         $markdown = $converter->toHtml();
 
