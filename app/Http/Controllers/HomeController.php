16481b987825fad8b7ce26da38cb91087a2e2cc4
ViskCY <109294364+ViskCY@users.noreply.github.com>
2024-08-01 23:25:04 +0300

    fix get right tag

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,11 +10,18 @@ class HomeController extends Controller
     public function index()
     {
         $categories = Category::all()->map(function ($category) {
+
+            // Log the category ID to debug
+            \Log::info('Category ID: ' . $category->id);
+
             // Get a random post image for the category using a raw query
             $randomPostImage = DB::table('posts')
-                // ->whereJsonContains('tags', $category->id)
+                ->whereJsonContains('tags', json_encode($category->id))
                 ->inRandomOrder()
                 ->value('featured_image');
+
+            // Log the random post image to debug
+            \Log::info('Random Post Image for Category ID ' . $category->id . ': ' . $randomPostImage);
 
             $category->background_image = $randomPostImage ?: asset('storage/default.jpg'); // default image if no post found
             return $category;
