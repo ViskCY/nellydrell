809dfa108f4723f8f1121ec3a05db9b934a3245d
ViskCY <109294364+ViskCY@users.noreply.github.com>
2024-08-01 22:26:59 +0300

    AboutController locale check

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -2,17 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\App;
 use App\Http\Controllers\MarkdownConverter;
 
 class AboutController extends Controller
 {
     public function index()
     {
-        $locale = session('language');
-        $markdownContent = file_get_contents(public_path("storage/{$locale}_about.md"));
+        $locale = App::getLocale();
+        $filePath = public_path("storage/{$locale}/about.md");
+        
+        if (!file_exists($filePath)) {
+            abort(404, "About file not found for locale: {$locale}");
+        }
+        
+        $markdownContent = file_get_contents(public_path("storage/{$locale}/about.md"));
         $converter = new MarkdownConverter($markdownContent);
         $markdown = $converter->toHtml();
 
         return view('about', ['markdown' => $markdown]);
     }
 }
+
