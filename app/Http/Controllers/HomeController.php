bcaae68e713efcccfe3a065eb4e39f77705f239b
ViskCY <109294364+ViskCY@users.noreply.github.com>
2024-08-01 23:05:16 +0300

    random post image

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,15 +2,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $categories = Category::all();
+        $categories = Category::all()->map(function($category) {
+            // Get a random post image for the category
+            $randomPost = Post::whereJsonContains('tags', $category->id)->inRandomOrder()->first();
+            $category->background_image = $randomPost ? $randomPost->image_url : 'default-image.jpg'; // default image if no post found
+            return $category;
+        });
 
         return view('welcome', compact('categories'));
     }
-    
 }
