57fa491bb4b33d55c9ee050b55b9d03a4f40b23a
ViskCY <109294364+ViskCY@users.noreply.github.com>
2024-08-05 22:39:28 +0300

    and sql eng support

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App; // Import the App facade for locale
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $categories = Category::all()->map(function ($category) {
+        $locale = App::getLocale(); // Get the current locale
+
+        $categories = Category::all()->map(function ($category) use ($locale) {
 
             // Log the category ID to debug
             \Log::info('Category ID: ' . $category->id);
@@ -22,6 +25,11 @@ class HomeController extends Controller
 
             // Log the random post image to debug
             \Log::info('Random Post Image for Category ID ' . $category->id . ': ' . $randomPostImage);
+
+            // Set the name based on the current locale
+            if ($locale === 'en') {
+                $category->name = $category->name_en;
+            }
 
             $category->background_image = $randomPostImage ?: asset('storage/default.jpg'); // default image if no post found
             return $category;
