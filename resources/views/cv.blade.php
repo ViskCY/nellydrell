151f7da45ae9777a7faf80f917ae438a8c6df10b
ViskCY <109294364+ViskCY@users.noreply.github.com>
2024-08-05 22:00:33 +0300

    move to other div

--- resources/views/cv.blade.php
+++ resources/views/cv.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
-    <div class="relative flex items-center justify-center h-screen">
-        <section itemprop="articleBody" class="prose dark:prose-invert pb-3 mt-10 sm:px-5">
+    <div class="relative flex items-center justify-center h-screen sm:px-5">
+        <section itemprop="articleBody" class="prose dark:prose-invert pb-3 mt-10">
             {!! $markdown !!}
         </section>
     </div>
