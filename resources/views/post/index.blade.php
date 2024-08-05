6711ce57593c5e4878b7ba5649c1e72913e61fff
ViskCY <109294364+ViskCY@users.noreply.github.com>
2024-08-05 23:15:38 +0300

    bug fix revert

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="title">
-        {{ $title ?? {{__('messages.pics')}} }}
+        {{ $title ?? 'Pildid' }}
     </x-slot>
 
     <div class="relative flex items-top justify-center sm:items-center py-4 sm:pt-0">
