7c4b71306f352166bcbb7dffbfd367fd59b74879
ViskCY <109294364+ViskCY@users.noreply.github.com>
2024-08-05 22:04:03 +0300

    fix padding on other pages

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
-    <div class="relative flex items-center justify-center h-screen">
-        <section itemprop="articleBody" class="prose dark:prose-invert pb-3 mt-10 sm:px-5">
+    <div class="relative flex items-center justify-center h-screen px-4 sm:px-6 lg:px-8">
+        <section itemprop="articleBody" class="prose dark:prose-invert pb-3 mt-10 w-full max-w-screen-sm mx-auto">
             {!! $markdown !!}
         </section>
     </div>
